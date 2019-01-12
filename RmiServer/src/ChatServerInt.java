7bad28b5e62c0c303e96ce46524b99d721f4d2e9
gdoroz <grzegorz.doroz@gmail.com>
2019-01-12 21:50:14 +0100

    Initial

--- RmiServer/src/ChatServerInt.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-import java.util.*;
-
-public interface ChatServerInt extends Remote{	
-	public boolean login(ChatClientInt a)throws RemoteException ;
-	public void publish(String s)throws RemoteException ;
-	public Vector getConnected() throws RemoteException ;
-}
